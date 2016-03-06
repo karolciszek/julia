954ee1f018522fe0b0441e3c4d8fd189fd8aad4f
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-03-06 15:43:41 +0000

    mandelbrot-gen.py: cleaned up; todo: introduce Julia sets & palette functions

--- mandelbrot-gen.py
+++ mandelbrot-gen.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import numpy as np
 import scipy.misc
 
@@ -9,26 +10,24 @@ ITERS_PER_POINT = 200
 def linear_interpolation(iteration):
     linInp = np.floor(255 * (iteration / ITERS_PER_POINT))
     return [linInp, linInp, linInp]
+linear_interpolation = np.vectorize(linear_interpolation)
 
 def smooth_interpolation(iteration, escaped):
     log2 = np.log(2)
     mu = iteration + 1 - np.log(np.log(np.abs(escaped))) / log2
     col_base = np.floor(255 * mu / ITERS_PER_POINT) if (not np.isnan(mu)) else 0
-    return [col_base, col_base, col_base]
+    return col_base
+smooth_interpolation = np.vectorize(smooth_interpolation)
 
 iterations = np.load(ITERATION_FILE)
 escaped_values = np.load(ESCAPED_FILE)
 
-(width, half_height) = iterations.shape
+(width, height) = iterations.shape
 # scipy saves bitmaps in form (height, width, colour_channels)
-middleRowIndex = half_height - 1
-bitmap_dimensions = (2 * half_height - 1, width, 3)
+bitmap_dimensions = (height, width, 3)
 bitmap = np.empty(bitmap_dimensions)
 
-for x in range(width):
-    for y in range(half_height):
-        color = smooth_interpolation(iterations[x][y], escaped_values[x][y])
-        bitmap[middleRowIndex + y][x] = color
-        bitmap[middleRowIndex - y][x] = color
+bitmap = smooth_interpolation(iterations, escaped_values)
+print(bitmap)
 
 scipy.misc.imsave(OUTPUT_FILE, bitmap)
