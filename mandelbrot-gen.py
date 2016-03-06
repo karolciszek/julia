02a0cd8cfe15c274a424aa30aefcd5f8ad2dd219
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-03-06 13:34:47 +0000

    mandelbrot: separated function from mandelbrot-gen

--- mandelbrot-gen.py
+++ mandelbrot-gen.py
@@ -1,21 +1,34 @@
 import numpy as np
 import scipy.misc
 
-FILENAME = '3840.npy'
+ITERATION_FILE = 'iter.npy'
+ESCAPED_FILE = 'escaped.npy'
 OUTPUT_FILE = 'out.png'
 ITERS_PER_POINT = 200
 
-def interpolate_color(iteration):
+def linear_interpolation(iteration):
     linInp = np.floor(255 * (iteration / ITERS_PER_POINT))
     return [linInp, linInp, linInp]
 
-mandelbrot_data = np.load(FILENAME)
-(width, height) = mandelbrot_data.shape
-bitmap = np.empty((width, height, 3))
+def smooth_interpolation(iteration, escaped):
+    log2 = np.log(2)
+    mu = iteration + 1 - np.log(np.log(np.abs(escaped))) / log2
+    col_base = np.floor(255 * mu / ITERS_PER_POINT) if (not np.isnan(mu)) else 0
+    return [col_base, col_base, col_base]
+
+iterations = np.load(ITERATION_FILE)
+escaped_values = np.load(ESCAPED_FILE)
+
+(width, half_height) = iterations.shape
+# scipy saves bitmaps in form (height, width, colour_channels)
+middleRowIndex = half_height - 1
+bitmap_dimensions = (2 * half_height - 1, width, 3)
+bitmap = np.empty(bitmap_dimensions)
 
 for x in range(width):
-    for y in range(height):
-        color = interpolate_color(mandelbrot_data[x][y])
-        bitmap[x][y] = color
+    for y in range(half_height):
+        color = smooth_interpolation(iterations[x][y], escaped_values[x][y])
+        bitmap[middleRowIndex + y][x] = color
+        bitmap[middleRowIndex - y][x] = color
 
 scipy.misc.imsave(OUTPUT_FILE, bitmap)
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,46 +1,43 @@
 import numpy as np
-#import matplotlib as mpl
-#import scipy.misc
 
-iterationsPerPoint = 200
+iterations_per_point = 200
 EPSILON = 0.01
 
-xMin = -2.1
-xMax = 0.7
-xHeight = 3840
-yMin = 0-1.2j
-yMax = 0+1.2j
+ITER_FILE = 'iter.npy'
+ESCAPED_FILE = 'escaped.npy'
+
+x_min = -2.1
+x_max = 0.7
+width = 3840
+y_min = 0
+y_max = 0+1.2j
 # preserving proportions in pixelheight
-yHeight = np.int(np.abs(yMax - yMin) * xHeight / (xMax - xMin))
+height = np.int(np.abs(y_max - y_min) * width / (x_max - x_min))
 
 # numPy data type to store complex numbers
 complex128bit = np.dtype('c16')
-reals = np.linspace(xMin, xMax, xHeight)
-imags = np.linspace(yMin, yMax, yHeight)
+reals = np.linspace(x_min, x_max, width)
+imags = np.linspace(y_min, y_max, height)
 
-def iterationsToEscape(c):
+def iterations_to_escape(c):
     """Computes the Mandelbrot sequence for complex c"""
     z = 0
     #print("[{0}]".format(c))
-    for i in range(iterationsPerPoint):
+    for i in range(iterations_per_point):
         z = z ** 2 + c
-        if np.abs(z) >= 2: return i
-    return 0
+        if np.abs(z) >= 2: break
+    return (i, z)
 
-def interpolateColor(iteration):
-    linInp = np.floor(255 * (iteration / iterationsPerPoint))
-    return [linInp, linInp, linInp]
+size = (width, height)
+iterations = np.empty(size)
+escaped_values = np.empty(size, complex128bit)
 
+for x in range(width):
+    if (x % 100 == 0): print x
+    for y in range(height):
+        (i, z) = iterations_to_escape(reals[x] + imags[y])
+        iterations[x][y] = i
+        escaped_values[x][y] = z
 
-# colour represented as int value between 0 and 255
-# there is a bitmap for each colour channel
-# bitmap = np.empty((yHeight, xHeight, 3))
-
-for y in range(yHeight):
-    for x in range(xHeight):
-        iteration = iterationsToEscape(reals[x] + imags[y])
-        print("({0},{1}),{2},[{3},{4}]".format(x, y, iteration, reals[x], imags[y]))
-        # colour = interpolateColor(iteration)
-        # bitmap[y][x] = colour
-
-# scipy.misc.imsave("out.png", bitmap)
+np.save(ITER_FILE, iterations)
+np.save(ESCAPED_FILE, escaped_values)
